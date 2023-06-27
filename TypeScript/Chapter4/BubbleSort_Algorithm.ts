5419230e3e594a7994bbcb0a22299f75777a69b4
Konadu Akwasi Akuoko <konaduakwasiakuoko@gmail.com>
2023-06-27 09:52:35 +0000

    undone

---
+++ TypeScript/Chapter4/BubbleSort_Algorithm.ts
@@ -0,0 +1,12 @@
+function BubbleSort(arr: number[]) {
+  let unsortedUntilIndex = arr.length - 1;
+  let sorted = false;
+  while (!sorted) {
+    sorted = true;
+    arr.forEach((item, index, arr) => {
+        if(item > arr[index + 1]){
+            
+        }
+    })
+  }
+}
