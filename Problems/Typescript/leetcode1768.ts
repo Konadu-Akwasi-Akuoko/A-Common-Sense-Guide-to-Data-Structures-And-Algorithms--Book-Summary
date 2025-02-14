5461e1977bc9f7adf18a0fb4b9bb6d5a505f6977
Akwasi Konadu Akuoko <konaduakwasiakuoko@gmail.com>
2025-02-14 16:19:17 +0000

    feat: ai solution for leetcode #2239

--- Problems/Typescript/leetcode1768.ts
+++ Problems/Typescript/leetcode1768.ts
@@ -52,3 +52,17 @@ function mergeAlternately(word1: string, word2: string): string {
 
   return mergeString;
 }
+
+// AI approach
+function mergeAlternately_ai(word1: string, word2: string): string {
+  let result = "";
+  let i = 0;
+  let j = 0;
+
+  while (i < word1.length || j < word2.length) {
+    if (i < word1.length) result += word1[i++];
+    if (j < word2.length) result += word2[j++];
+  }
+
+  return result;
+}
